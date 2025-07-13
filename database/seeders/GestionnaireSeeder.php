cb2adbec6a18cfb8ca0d9f060781185e31366ce9
PapeNdiagaFAYE <fayepapendiaga39@gmail.com>
2025-07-13 09:48:30 +0000

    Premier commit - Examen_Laravel

---
+++ database/seeders/GestionnaireSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class GestionnaireSeeder extends Seeder
+{
+    public function run()
+    {
+        // Vérifier s'il existe déjà un gestionnaire, sinon en créer un
+        if (User::where('role', 'gestionnaire')->count() == 0) {
+            User::create([
+                'name' => 'PapeSam',
+                'email' => 'papesam@ipp-burger.com',
+                'password' => 'Passer17#', // Assure-toi de changer ce mot de passe
+                'role' => 'gestionnaire',
+            ]);
+        }
+    }
+}
